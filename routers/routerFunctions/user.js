90433203677545ad7a3a08ac802fca600fa084a0
2Roy2 <royarad5@gmail.com>
2022-01-22 22:36:41 +0200

    added sereate file for user funcs

---
+++ routers/routerFunctions/user.js
@@ -0,0 +1,45 @@
+const getAllUsers=async (req, res) => {
+    User.find().
+        then((result) => {
+            res.json(result)
+        })
+        .catch((err) => {
+            res.json({ err })
+        })
+}
+
+const addUser=async (req, res) => {
+    const { name, password } = req.body
+    if (!name || !password)
+        return res.status(400).json({ err: "no name or password" });
+
+    //check if user with same name already exists
+    const existingUser=await User.findOne({name:name})
+    .then((result)=>{
+        if(result)
+        return {err:"user already exists"}
+    })
+    .catch((err) => {
+        res.json({ err })
+    })
+    if(existingUser)
+        return res.json(existingUser)
+    
+    //create user
+    const user = User({
+        name: name,
+        password: password,
+        recipe: []
+    });
+    user.save().then((result) => {
+        res.json(result);
+    })
+        .catch((err) => {
+            res.json({ err })
+        });
+}
+
+module.exports={
+    getAllUsers,
+    addUser
+}
--- routers/user.js
+++ routers/user.js
@@ -1,54 +1,16 @@
 const express = require("express")
 const User = require("../models/user")
+const {getAllUsers, addUser}=require('./routerFunctions/user')
+
 const router = express.Router()
 
 // Get all posts
-router.get('/', async (req, res) => {
-    User.find().
-        then((result) => {
-            res.json(result)
-        })
-        .catch((err) => {
-            res.json({ err })
-        })
-})
+router.get('/',getAllUsers )
 
 
-router.post('/', async (req, res) => {
-    const { name, password } = req.body
-    if (!name || !password)
-        return res.status(400).json({ err: "no name or password" });
+router.post('/', addUser)
 
-    //check if user with same name already exists
-    const existingUser=await User.findOne({name:name})
-    .then((result)=>{
-        if(result)
-        return {err:"user already exists"}
-    })
-    .catch((err) => {
-        res.json({ err })
-    })
-    if(existingUser)
-        return res.json(existingUser)
-    
-    //create user
-    const user = User({
-        name: name,
-        password: password,
-        recipe: []
-    });
-    user.save().then((result) => {
-        res.json(result);
-    })
-        .catch((err) => {
-            res.json({ err })
-        });
-})
-
-router.get('/:name',(req, res)=>{
-
-
-})
+router.get('/:name',)
 
 module.exports = router
 
