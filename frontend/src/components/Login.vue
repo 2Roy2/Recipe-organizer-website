b3e94ceba2042b6018307d3cc2e515133268ea32
2Roy2 <royarad5@gmail.com>
2022-01-30 16:12:10 +0200

    shit

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container border mt-4 w-25">
+    <div class="mb-3 mt-3">
+      <label for="username" class="form-label">username</label>
+      <input type="text" class="form-control" v-model="username" />
+    </div>
+    <div class="mb-5">
+      <label for="password" class="form-label">password</label>
+      <input type="password" class="form-control" v-model="password" />
+    </div>
+    <div class="d-grid gap-2 col-6 mx-auto">
+        <button @click="onClick" type="button " class="btn btn-success">login</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data(){
+      return{
+          username:'',
+          password:''
+      }
+  },
+  methods:{
+      onClick(){
+          console.log(this.password)
+          console.log(this.username)
+      }
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav
+    class="navbar navbar-expand-lg navbar-dark"
+    style="background-color: #40c965"
+  >
+    <div class="container-fluid">
+      <a class="navbar-brand m-3">Recipe organiser</a>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <button type="button" class="btn btn-outline-light ms-3 me-3">
+              login
+            </button>
+          </li>
+          <li class="nav-item">
+            <button type="button" class="btn btn-outline-light ms-3 me-3">
+              logout
+            </button>
+          </li>
+          <li class="nav-item">
+            <button type="button" class="btn btn-outline-light ms-3 me-5">
+              register
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <login></login>
+</template>
+
+<script>
+import Login from "./Login.vue";
+
+export default {
+  name: "NavBar",
+  components: {
+    Login,
+  },
+};
+</script>
+
+<style scoped>
+</style>
